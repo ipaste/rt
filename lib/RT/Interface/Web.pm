d7ee8a9f384ae88e43c947ccd867352c80aadf81
Craig Kaiser <craig@bestpractical.com>
2020-02-04 18:51:09 -0500

    Ensure lifecycle cache is updated across threads when flagged

--- lib/RT/Interface/Web.pm
+++ lib/RT/Interface/Web.pm
@@ -279,6 +279,8 @@ sub HandleRequest {
 
     RT->Config->RefreshConfigFromDatabase();
 
+    MaybeRebuildLifecycleCache();
+
     $HTML::Mason::Commands::r->content_type("text/html; charset=utf-8");
 
     $HTML::Mason::Commands::m->{'rt_base_time'} = [ Time::HiRes::gettimeofday() ];
@@ -1286,6 +1288,15 @@ sub MaybeRebuildCustomRolesCache {
     if ($needs_update > $role_cache_time) {
         RT::CustomRoles->RegisterRoles;
         $role_cache_time = $needs_update;
+    }
+}
+
+my $lifecycle_cache_time = time;
+sub MaybeRebuildLifecycleCache {
+    my $needs_update =  RT->System->LifecycleCacheNeedsUpdate;
+    if ( $needs_update > $lifecycle_cache_time ) {
+        RT::Lifecycle->FillCache;
+        $lifecycle_cache_time = $needs_update;
     }
 }
 
